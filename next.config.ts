b27500369a302c01c2318e2e7a36377af9c44978
Sergio Vasconcelos <pastaengear@gmail.com>
2026-01-27 02:10:02 +0000

    Não deu certo.... O problema persiste

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  generateBuildId: async () => {
+    // This will create a new build id every time, forcing a clean build.
+    return new Date().getTime().toString();
+  },
 };
 
 export default nextConfig;
