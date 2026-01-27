e89bf5afc9e1d6950ec526baa360b46d8046b11e
Sergio Vasconcelos <pastaengear@gmail.com>
2026-01-27 14:40:47 +0000

    consigo colar aqui. segue acima

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,22 +1,12 @@
 // src/firebase/config.ts
 
-// POR FAVOR, SUBSTITUA O CONTEÚDO ABAIXO
-// com a configuração do seu novo projeto Firebase.
-//
-// Você pode encontrá-la seguindo estes passos:
-// 1. Vá para o Console do Firebase: https://console.firebase.google.com/
-// 2. Selecione seu projeto "ENGEAR - Depto Comercial - V3".
-// 3. No menu à esquerda, clique na engrenagem (ao lado de "Visão geral do projeto") e selecione "Configurações do projeto".
-// 4. Na aba "Geral", role para baixo até a seção "Seus aplicativos".
-// 5. Se não houver um aplicativo da Web (com um ícone </>), clique no ícone </> para criar um. Dê um apelido (ex: "App Web") e clique em "Registrar aplicativo".
-// 6. Na etapa "Adicionar SDK do Firebase", você verá um objeto chamado "firebaseConfig". Copie todo o objeto.
-// 7. Cole o objeto que você copiou para substituir TUDO a partir da linha "export const firebaseConfig = ..." abaixo.
+// O CONTEÚDO ABAIXO FOI ATUALIZADO COM A CONFIGURAÇÃO FORNECIDA.
 
 export const firebaseConfig = {
-  apiKey: "COLE_SUA_API_KEY_AQUI",
-  authDomain: "COLE_SEU_AUTH_DOMAIN_AQUI",
-  projectId: "COLE_SEU_PROJECT_ID_AQUI",
-  storageBucket: "COLE_SEU_STORAGE_BUCKET_AQUI",
-  messagingSenderId: "COLE_SEU_MESSAGING_SENDER_ID_AQUI",
-  appId: "COLE_SEU_APP_ID_AQUI"
+  apiKey: "AIzaSyAhxJd_qG2_A2-5sOYcRz9Wd4o1PBrqSnU",
+  authDomain: "engear-depto-comercial-v3.firebaseapp.com",
+  projectId: "engear-depto-comercial-v3",
+  storageBucket: "engear-depto-comercial-v3.appspot.com",
+  messagingSenderId: "93127815316",
+  appId: "1:93127815316:web:61a49c693a1f942971295b"
 };
