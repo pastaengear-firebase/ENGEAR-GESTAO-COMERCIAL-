80128b492a225af757beb7c7a3c26a4e82638f0b
Sergio Vasconcelos <pastaengear@gmail.com>
2026-01-27 19:24:23 +0000

    Investigue novamente o pq de nao conseguir publicar no plano spark.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Configura o Next.js para gerar um site estático na pasta 'out'
+  output: 'export',
+
+  // Adiciona uma barra final (ex: /sobre/) aos links. 
+  // Isso melhora a compatibilidade com hospedagens de sites estáticos.
+  trailingSlash: true,
+};
 
 export default nextConfig;
