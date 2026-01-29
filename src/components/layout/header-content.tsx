6760864471e31596e25ecd06ba636a1d6773d489
Sergio Vasconcelos <pastaengear@gmail.com>
2026-01-29 16:06:48 +0000

    agora sim, mas nao gostei da cor....ajuste a para o amarelo da nossa pal

--- src/components/layout/header-content.tsx
+++ src/components/layout/header-content.tsx
@@ -30,7 +30,7 @@ export default function HeaderContent({ toggleMobileMenu }: HeaderContentProps) 
   };
 
   return (
-    <header className="sticky top-0 z-30 w-full border-b bg-[hsl(43_88%_90%)] dark:bg-[hsl(43_25%_15%)]">
+    <header className="sticky top-0 z-30 w-full border-b bg-accent">
       <div className="container flex h-10 items-center justify-between px-4 sm:px-6 lg:px-8">
         <div className="flex items-center">
           <Button variant="ghost" size="icon" onClick={toggleMobileMenu} className="mr-2 md:hidden text-sidebar-foreground hover:bg-sidebar-accent hover:text-sidebar-accent-foreground">
@@ -83,7 +83,7 @@ export default function HeaderContent({ toggleMobileMenu }: HeaderContentProps) 
 
         </div>
       </div>
-      <div className="md:hidden border-t p-1 flex justify-center bg-[hsl(43_88%_90%)] dark:bg-[hsl(43_25%_15%)] text-sm font-medium text-sidebar-foreground">
+      <div className="md:hidden border-t p-1 flex justify-center bg-accent text-sm font-medium text-sidebar-foreground">
          <UserIcon className="h-5 w-5 mr-2" />
          <span>{selectedSeller}</span>
       </div>
