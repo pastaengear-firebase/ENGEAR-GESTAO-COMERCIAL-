c27aac1234e92b0f00e994cae454b68436d8fa79
Sergio Vasconcelos <pastaengear@gmail.com>
2026-02-01 15:23:39 +0000

    e essa

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,9 +5,9 @@
 
 export const firebaseConfig = {
   apiKey: "AIzaSyAfGwU56XJvloOMDig5aljP8lN1hVnK9RM",
-  authDomain: "engear-depto-comercial-v3.firebaseapp.com",
-  projectId: "engear-depto-comercial-v3",
-  storageBucket: "engear-depto-comercial-v3.appspot.com",
+  authDomain: "engear---depto-comercial-v3.firebaseapp.com",
+  projectId: "engear---depto-comercial-v3",
+  storageBucket: "engear---depto-comercial-v3.appspot.com",
   messagingSenderId: "297634043634",
   appId: "1:297634043634:web:7ab55b978129187c9aa26b",
   measurementId: "G-XXEKVJLWDG"
