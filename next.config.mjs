68bf760fb9647a27f457a5051135dc78d74d000c
Sergio Vasconcelos <pastaengear@gmail.com>
2026-02-01 03:50:32 +0000

    Vc já fez essa solução acima, vc não está atento....eu vou esperar mais

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // O seu output anterior, agora removido: output: 'export',
-  // O seu images.unoptimized anterior, agora removido: images: { unoptimized: true },
+    output: 'export',
 };
 
 export default nextConfig;
